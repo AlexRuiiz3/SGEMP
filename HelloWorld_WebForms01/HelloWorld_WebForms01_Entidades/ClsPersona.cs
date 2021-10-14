862b30250b7fbf935267f0190ac6cccaa1f38cfd
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2021-10-14 14:32:13 +0200

    ASP .net Hello World

---
+++ HelloWorld_WebForms01/HelloWorld_WebForms01_Entidades/ClsPersona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace HelloWorld_WebForms01_Entidades
+{
+    public class ClsPersona
+    {
+
+        #region Atributos privados
+        private String nombre;
+        #endregion
+
+
+        #region Constructores
+        //Constructro por defecto
+        public ClsPersona() {
+            nombre = "";
+        }
+
+        //Constructor con parametros
+        public ClsPersona(String nombre) {
+
+            this.nombre = nombre;
+        }
+        #endregion
+
+
+        #region Propiedades publicas 
+        //Nombre
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        #endregion
+
+        /* Otras formas
+         * 
+         * 1.
+         *
+         *   private String nombre;
+         *   public String GetNombre() => nombre;
+         *   public void SetNombre(String nombre) { this.nombre = nombre; }* private String nombre;
+         * 
+         *
+         * 
+         * 2.Propiedades auto implementadas //Son las que luego en el get y en el set no se va a introducir mas codigo
+         * public String nombre{get,set} //por defecto se crea un private String nombre
+         * 
+         * 
+         * 3.public String nombre {get => nombre; set => nombre = value;}
+         */
+
+
+
+    }
+}
---
+++ HelloWorld_WebForms01/HelloWorld_WebForms01_UI/Views/WebFormFormulario.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HelloWorld_WebForms01_Entidades;
+
+namespace HelloWorld_WebForms01_UI.Views
+{
+    public partial class WebFormFormulario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
