670c61cb0c5373913e57f984036a0e0dfaddabdf
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2021-10-26 13:54:39 +0200

    Unidad 6 Act 4 terminado y Unidad 7 act 1

---
+++ Unidad7/Actividad1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Actividad1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            ViewData["MensajeSaludo"] = Utilidad.obtenerMensaje();
+            ViewBag.fechaActual = DateTime.Now.ToString();
+
+            return View();
+        }
+
+    }
+}
---
+++ Unidad7/Actividad1/Gestion/Utilidad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Actividad1.Models
+{
+    public class Utilidad
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static String obtenerMensaje()
+        {
+            String mensaje = "Buenas noches";
+
+            DateTime fechaActual = DateTime.Now;
+
+            if (fechaActual.Hour >= 6 && fechaActual.Hour < 12) {
+                mensaje = "Buenos Dias";
+            } else if (fechaActual.Hour >= 12 && fechaActual.Hour < 22)
+            {
+                mensaje = "Buenas tardes";
+            }
+
+            return mensaje;
+        }
+
+    }
+}
