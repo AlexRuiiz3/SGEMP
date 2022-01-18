301979086a411d26a783cafd45fda61bdff465d3
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2022-01-18 13:45:02 +0100

    Unidad 13 Ejercicio 1 y 2

---
+++ Unidad13 AJAX/js/Ejercicio1.js
@@ -0,0 +1,20 @@
+﻿window.onload = inicializar;
+
+function inicializar() {
+
+    document.getElementById("btnSaludar").addEventListener("click", saludar,false);
+
+}
+
+function saludar() {
+    var llamada = new XMLHttpRequest();
+    llamada.open("GET", "Hola.html");
+    llamada.onreadystatechange = function () {
+
+        if (llamada.readyState == 4 && llamada.status == 200) {
+            document.getElementById("divSaludo").innerHTML = llamada.responseText;
+        }
+    }
+    llamada.send();
+
+}
---
+++ Unidad13 AJAX/js/Ejercicio2.js
@@ -0,0 +1,21 @@
+﻿window.onload = inicializar;
+
+function inicializar() {
+
+    document.getElementById("btnEnviar").addEventListener("click", obtenerApellido, false);
+
+}
+
+function obtenerApellido() {
+    var llamada = new XMLHttpRequest();
+    llamada.open("GET", "https://crudpersonasasp-alexruiz.azurewebsites.net/api/personas");
+    llamada.onreadystatechange = function () {
+
+        if (llamada.readyState == 4 && llamada.status == 200) {
+            var arrayPersonas = JSON.parse(llamada.responseText);
+            document.getElementById("divApellidos").innerHTML = arrayPersonas[0].apellidos;
+        }
+    }
+    llamada.send();
+
+}
