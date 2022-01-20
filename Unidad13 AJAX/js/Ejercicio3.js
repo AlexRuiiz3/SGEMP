66b14ce28496b95f4cf1ab7ac274e653896b6772
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2022-01-20 14:30:44 +0100

    .

---
+++ Unidad13 AJAX/js/Ejercicio3.js
@@ -0,0 +1,26 @@
+﻿window.onload = inicializar;
+
+function inicializar() {
+    document.getElementById("btnBorrar").addEventListener("click", borrar,false);
+
+}
+
+function borrar() {
+    var id = document.getElementById("txtBox").value;
+    var peticion = new XMLHttpRequest();
+    peticion.open("DELETE", "https://crudpersonasasp-alexruiz.azurewebsites.net/api/personas/"+id);
+
+    peticion.onreadystatechange = function () {
+
+        if (peticion.readyState < 4) {
+
+        } else {
+            if (peticion.status == 200) {
+                document.getElementById("divMensaje").innerHTML = "Persona eliminada con exito";
+            } else if (peticion.status == 404) {
+                document.getElementById("divMensaje").innerHTML = "Persona no encontrada";
+            }
+        }
+    }
+    peticion.send();
+}
